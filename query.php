5432a417ba66a8024cf58d06f34d98014c8a0ce8
Varad Gaikwad <91328424+gvarad@users.noreply.github.com>
2024-01-29 18:49:39 +0530

    Add files via upload

---
+++ query.php
@@ -0,0 +1,22 @@
+<?php
+require "includes/common.php";
+session_start();
+?>
+
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+    <div class="alert alert-success">
+  <strong>Success!</strong> Your Query has been Submitted Successfully we will get in touch with you as soon as possible.
+</div>
+<div class="text-center">
+<button type="button" class="btn btn-secondary text-center" ><a href="index.php" style="color:white;">Go Home</a></button>
+</div>
+
+</body>
+
+</html>
